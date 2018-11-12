88032ddfb689b9d1888f6906b954ef7336d9af11
dougtq <douglas.expalves@gmail.com>
2018-11-12 17:06:21 -0200

    exemplos de comentário

--- fundamentos/primeiro/primeiro.go
+++ fundamentos/primeiro/primeiro.go
@@ -1,7 +1,13 @@
 package main
 
+// 	Use o import para usar os pacotes go
 import "fmt"
 
+/*
+	Comentários
+*/
+
+// Função padrão de inicio do go é a main c-like
 func main() {
 	fmt.Println("Hello, World!")
 	fmt.Println("Primeiro programa...")
