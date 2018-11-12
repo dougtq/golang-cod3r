08b1d0130f947b37a5ec8b2b3271e8b932a825e7
dougtq <douglas.expalves@gmail.com>
2018-11-12 17:27:37 -0200

    explorando fmt

---
+++ fundamentos/fmt-lib/prints.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Print("Mesma")
+	fmt.Print("Linha ")
+
+	fmt.Println("Nova linha")
+	fmt.Println("Outra linha")
+
+	x := 3.141516
+	xs := fmt.Sprint(x)
+
+	fmt.Println("O valor de x é " + xs)
+	fmt.Println("O valor de x é ", x)
+
+	fmt.Printf("O valor de x é %.2f", x)
+
+}
